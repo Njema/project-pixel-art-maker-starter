ca988c9939ad0a9fd5dba798eb4ec0e4212884c0
Rispher <njemarispher24@gmail.com>
2018-06-27 13:48:04 +0300

    Designing Pixel Canvas

--- designs.js
+++ designs.js
@@ -4,7 +4,40 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+	let height = document.getElementById("inputHeight").value;
+	let width = document.getElementById("inputWeight").value;
+	let table = document.getElementById("pixelCanvas");
 
-// Your code goes here!
+	table.innerHTML = "";
 
+	let tbody = document.createElement("tbody");
+
+	for (let i = 0; i < width; i++)
+	{
+      
+      let tr = document.createElement("tr");
+
+      for(let j = 0; j < width; j++) {
+
+      	let td = document.createElement("td");
+      	td.appendChild(document.createTextNode(""));
+      	tr.appendChild(td);
+      }
+
+      	tbody.appendChild(tr);
+	}
+   
+   table.appendChild(tbody);
 }
+
+$("body").on ("click", "td", function(){
+
+	 let color = document.getElementById("colorPicker").value;
+	 
+	 if ($(this).attr("style")) {
+
+	 	$(this).removeAttr("style");
+	 } else {
+	 	 $(this).css("background-color",color);
+	 }
+});
